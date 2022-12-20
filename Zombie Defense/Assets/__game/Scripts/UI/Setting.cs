d49c2982ec0fcda8b0f5fa0c8b0118a8cac047bb
Nguyễn Công Quyền <quyen.nc207699@sis.hust.edu.vn>
2022-12-20 09:49:28 +0700

    setting

---
+++ Zombie Defense/Assets/__game/Scripts/UI/Setting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setting : UICanvas
+{
+    public override void Show(bool _isShown, bool isHideMain = true)
+    {
+        base.Show(_isShown, isHideMain);
+        if (isShow)
+        {
+        }
+    }
+}
